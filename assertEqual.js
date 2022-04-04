a347701ad07a9b96e5dc4a79c02d0f6d2b5d7fe0
firewallfail <firewall.fail@hotmail.com>
2022-04-04 15:55:45 -0400

    updated to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 //Function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
