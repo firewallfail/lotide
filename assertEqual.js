f9abaf6bef5003414f5e2ca6cd92d9d64e3bdb05
firewallfail <firewall.fail@hotmail.com>
2022-04-04 15:47:42 -0400

    Lotide assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,22 @@
+//Function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+//same numbers
+assertEqual(1, 1);
+//different numbers
+assertEqual(1, 17);
+//different types
+assertEqual(1, "Hello");
+//different strings
+assertEqual("2:", ":2");
+//same strings
+assertEqual("Hello", "Hello");
