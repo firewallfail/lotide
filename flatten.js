72ffb06c15f464a0b7617f9be1ee36744e3592d5
firewallfail <firewall.fail@hotmail.com>
2022-04-05 18:08:26 -0400

    Added flatten.js

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) return false;
+  return firstArray.every((value, index) => value === secondArray[index]);
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const flatten = function(arr) {
+  let flattenedArray = []
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      for (let smallElement of element) {
+        flattenedArray.push(smallElement);
+      }
+    }
+    else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray;
+};
+
+//test case
+const test1 = flatten([1, 2, 3, [4, 5, 6], 7, 8, [9, 10], 11, 12]);
+assertArraysEqual(test1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+const test2 = flatten([1, 2, 3, [4, 5, 6], 7, 8, [9, 10], 11, 12]);
+assertArraysEqual(test1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
