c3e911e1befa3e341e4575fc8cd95d528ed240ba
firewallfail <firewall.fail@hotmail.com>
2022-04-06 14:19:53 -0400

    implemented countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (inputString) => {
+  const countedLetters = {};
+  for (const letter of inputString) {
+    if (countedLetters[letter]) {
+      countedLetters[letter]++;
+    } else if (letter !== ' ') {
+      countedLetters[letter] = 1;
+    }
+  }
+  return countedLetters;
+};
+
+//Test code
+const test1 = countLetters('LHL');
+const test2 = countLetters("lighthouse in the house");
+console.log(test1);
+console.log(test2);
