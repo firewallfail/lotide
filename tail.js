a3c1ff3f75b4dadd5f057c4b2aa27073af09aa60
firewallfail <firewall.fail@hotmail.com>
2022-04-11 09:44:08 -0400

    Moved tests for tail into tailTest

--- tail.js
+++ tail.js
@@ -1,29 +1,5 @@
-//copy of assertEqual
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1, array.length);
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail([1]));
-console.log(tail([]));
-//check the array isn't modified
-console.log(words);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); //ensure 2 elements were returned
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log(result);
+module.exports = tail;
